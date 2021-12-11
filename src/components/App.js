5fb1fd68e365f3ccb0a6a977c39a1d8280f31dc6
gtiavo <victorg.avila@gmail.com>
2021-12-11 20:16:29 -0300

    agregue panel para el total de user, utilice fetch para pegarle a la api creada

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import Header from './partials/Header';
 import Footer from './partials/Footer';
+import PanelTotales from './paneles/PanelTotales';
 import '../assets/App.css';
 
 function App() {
   return (
     <div className="App">
    < Header />
+    < PanelTotales />
    < Footer />
     </div>
   );
---
+++ src/components/paneles/PanelTotales.js
@@ -0,0 +1,13 @@
+import Users from "./Users";
+
+function PanelTotales() {
+    return ( 
+        <section className='conteinerPanelTotal'>
+        < Users />
+        < Users />
+        < Users />
+        </section>
+     );
+}
+
+export default PanelTotales;
