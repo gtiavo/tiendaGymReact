42cd5db5eeceb9aa40044bf052d6a24cc7706ece
“ignacioamadeo” <ignacio.amadeo@gmail.com>
2022-01-05 20:37:23 -0300

    mensajes no guardados

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,12 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const {
+    createProxyMiddleware
+ } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-    app.use(
-      createProxyMiddleware(["/api", , "/otherApi"], { target: "http://localhost:5000" })
-    );
-  };
-  
+ module.exports = function(app) {
+    app.use(createProxyMiddleware('/api/**', {
+       target: 'http://localhost:3000'
+    }));
+    app.use(createProxyMiddleware('/otherApi/**', {
+       target: 'http://localhost:3000'
+    }));
+ };
